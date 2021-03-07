66f4af9006a1c3b4ce6b55fc4719746ada7d70ab
h-komatsu <mu.on.the.moon54078@gmail.com>
2021-03-07 19:01:46 +0900

    create template by Spring initializr

---
+++ src/test/java/com/topology/temperature/TakeYourTemperatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topology.temperature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeYourTemperatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
